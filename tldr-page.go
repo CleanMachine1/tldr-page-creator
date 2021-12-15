92aa39cc6ca2669c133a5a8dc77e1d01c868afb3
258204 <258204873@protonmail.com>
2021-12-15 16:49:03 -0700

    removed double spaces

--- tldr-page.go
+++ tldr-page.go
@@ -151,7 +151,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Saving to", string(colorBlue), path + "/" + pagename ,string(colorWhite), "and exiting.")
+	fmt.Print("Saving to ", string(colorBlue))
+	fmt.Print(path + "/" + pagename ,string(colorWhite))
+	fmt.Print(" and exiting.\n")
 	fmt.Println("If you want to contribute this page to TLDR, please follow the instructions ")
 	fmt.Println("from the following link:")
 	fmt.Print(string(colorBlue), "https://github.com/tldr-pages/tldr#how-do-i-contribute\n",string(colorWhite))
