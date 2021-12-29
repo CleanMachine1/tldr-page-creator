75fccd658a471fdf6249617db5ae2a2d3d4a3d5c
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-12-29 02:58:26 +0100

    rename variables, functions and consts to fit standard

--- tldr-page.go
+++ tldr-page.go
@@ -12,15 +12,15 @@ import (
 )
 
 const ( // Usage for program's version and ANSI codes for text formatting
-	version     = "v1.4"
-	colorWhite  = "\033[37m"
-	colorBlue   = "\033[36m"
-	colorYellow = "\033[33m"
-	Underline   = "\033[4m"
-	Normal      = "\033[0m"
+	VERSION     = "v1.4"
+	COLORWHITE  = "\033[37m"
+	COLORBLUE   = "\033[36m"
+	COLORYELLOW = "\033[33m"
+	UNDERLINE   = "\033[4m"
+	NORMAL      = "\033[0m"
 )
 
-func checkempty(input string) { // Function used to check if whether a string entered is empty/whitespace
+func Check_Empty(input string) { // Function used to check if whether a string entered is empty/whitespace
 	input = strings.ReplaceAll(input, " ", "") // if the string given is "      ", its still blank
 	if input == "" {
 		fmt.Println("Exiting, invalid input!")
@@ -28,14 +28,14 @@ func checkempty(input string) { // Function used to check if whether a string en
 	}
 }
 
-func reader() string { // Function for collecting user input easier as a string
+func Reader() string { // Function for collecting user input easier as a string
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	input := scanner.Text()
 	return input
 }
 
-func remove_punctuation(input string, choice int) string { // Function to fix errors regarding syntax
+func Remove_Punctuation(input string, choice int) string { // Function to fix errors regarding syntax
 	switch choice {
 	case 1:
 		temp := strings.Trim(input, ".:-># ") // without backticks
@@ -51,28 +51,28 @@ func remove_punctuation(input string, choice int) string { // Function to fix er
 }
 
 func main() {
-	doubledescflag := flag.Bool("2", false, "Use 2 lines in the description") // format: parameter, default, description
-	versionflag := flag.Bool("v", false, "Display version")
+	double_desc_flag := flag.Bool("2", false, "Use 2 lines in the description") // format: parameter, default, description
+	version_flag := flag.Bool("v", false, "Display version")
 	// -h comes with using the flag package using the descriptions of each flag
 	flag.Parse()
 
-	if *versionflag {
-		fmt.Println("TLDR-page version:", version)
+	if *version_flag {
+		fmt.Println("TLDR-page version:", VERSION)
 		os.Exit(0)
 	}
 
 	fmt.Println("Enter the name of the program/command:")
-	title1 := reader()                     // Uses bufio in a function to limit repeated code
-	checkempty(title1)                     // Check if title1 is whitespace/blank
-	title1 = remove_punctuation(title1, 1) // Removes the punctuation which the user could enter
+	title := Reader()                    // Uses bufio in a function to limit repeated code
+	Check_Empty(title)                   // Check if title is whitespace/blank
+	title = Remove_Punctuation(title, 1) // Removes the punctuation which the user could enter
 
-	pagename := strings.ReplaceAll(title1, " ", "-") + ".md" // for creating the file name
+	pagename := strings.ReplaceAll(title, " ", "-") + ".md" // for creating the file name
 
 	// If the command entered is (for example) git push, the white space will become - so therefore git-push.md
 
 	if _, err := os.Stat(pagename); err == nil { // Check if page exists before trying to overwrite it
-		fmt.Printf(string(colorYellow)+"file %q already exists, overwrite it? (y/N) "+string(colorWhite), pagename)
-		choice := reader()
+		fmt.Printf(string(COLORYELLOW)+"file %q already exists, overwrite it? (y/N) "+string(COLORWHITE), pagename)
+		choice := Reader()
 
 		if choice == "y" || choice == "yes" || choice == "Yes" {
 			os.Remove(pagename) // Delete the file, to be created later
@@ -82,28 +82,28 @@ func main() {
 		}
 
 	}
-	title1 = "# " + title1 // For when writing to page, for TLDR syntax
+	title = "# " + title // For when writing to page, for TLDR syntax
 
 	fmt.Println("Enter a description for the program/command:")
-	desc1 := reader()
-	checkempty(desc1)
-	desc1 = remove_punctuation(desc1, 1)
+	desc1 := Reader()
+	Check_Empty(desc1)
+	desc1 = Remove_Punctuation(desc1, 1)
 
-	desc := "> " + capitalise.First(desc1) + "."
+	final_desc := "> " + capitalise.First(desc1) + "."
 
-	if *doubledescflag { // If the flag is raised then:
+	if *double_desc_flag { // If the flag is raised then:
 		fmt.Println("Enter a second description for the program/command:")
-		desc2 := reader()
-		checkempty(desc2)
-		desc2 = remove_punctuation(desc2, 1)
+		desc2 := Reader()
+		Check_Empty(desc2)
+		desc2 = Remove_Punctuation(desc2, 1)
 
 		// Change the desc variable to include desc2
-		desc = desc + "\n> " + capitalise.First(desc2) + "."
+		final_desc = final_desc + "\n> " + capitalise.First(desc2) + "."
 	}
 
 	fmt.Println("Enter a more information link:")
-	link := reader()
-	checkempty(link)
+	link := Reader()
+	Check_Empty(link)
 	link = "> More information: <" + link + ">." // Formating
 
 	file, err := os.OpenFile(pagename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755) // Open the file
@@ -112,29 +112,29 @@ func main() {
 	}
 	defer file.Close() // Close the file as the final command
 
-	file.WriteString(title1 + "\n" + "\n" + desc + "\n" + link + "\n") // Write the title, desc, and link
+	file.WriteString(title + "\n" + "\n" + final_desc + "\n" + link + "\n") // Write the title, desc, and link
 
 	var i int // Assign empty variable
-	fmt.Println(string(colorBlue) + "MAX 8 commands, to exit and save early, enter no input at any stage." + string(colorWhite))
+	fmt.Println(string(COLORBLUE) + "MAX 8 commands, to exit and save early, enter no input at any stage." + string(COLORWHITE))
 	for i = 1; i <= 8; i++ { // commands part of the page - allows 8
 		fmt.Printf("Command example %d/8\n", i)
 
-		fmt.Println(string(colorBlue) + string(Underline) + "Part 1." + string(Normal) + " Enter a description for a command example:")
-		command_desc := reader()
-		command_desc = remove_punctuation(command_desc, 1)
+		fmt.Println(string(COLORBLUE) + string(UNDERLINE) + "Part 1." + string(NORMAL) + " Enter a description for a command example:")
+		command_desc := Reader()
+		command_desc = Remove_Punctuation(command_desc, 1)
 
 		command_desc = capitalise.First(command_desc)
 
 		if command_desc == "" { // Break to end if empty
-			// Wont use checkempty() since we don't want to exit
+			// Wont use Check_Empty() since we don't want to exit
 			break
 		}
 
 		command_desc = "- " + capitalise.First(command_desc) + ":"
 
-		fmt.Println(string(colorBlue) + string(Underline) + "Part 2." + string(Normal) + " Now enter the corresponding command:") // Part 2
-		command := reader()
-		command = remove_punctuation(command, 0) // with 0 because we need the backticks to be removed since users may enter them
+		fmt.Println(string(COLORBLUE) + string(UNDERLINE) + "Part 2." + string(NORMAL) + " Now enter the corresponding command:") // Part 2
+		command := Reader()
+		command = Remove_Punctuation(command, 0) // with 0 because we need the backticks to be removed since users may enter them
 
 		if command == "" { // Break to end
 			break
@@ -150,7 +150,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print("Saving to " + string(colorBlue) + path + "/" + pagename + " " + string(colorWhite) + "and exiting.\n")
+	fmt.Print("Saving to " + string(COLORBLUE) + path + "/" + pagename + " " + string(COLORWHITE) + "and exiting.\n")
 	fmt.Println("If you want to contribute this page to TLDR, please follow the instructions\nfrom the following link:")
-	fmt.Print(string(colorBlue) + "https://github.com/tldr-pages/tldr#how-do-i-contribute\n" + string(colorWhite))
+	fmt.Print(string(COLORBLUE) + "https://github.com/tldr-pages/tldr#how-do-i-contribute\n" + string(COLORWHITE))
 }
